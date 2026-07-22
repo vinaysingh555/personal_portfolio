4500b8481f429866a6c38b5a2e437b1cfbc6b7be
vinaysingh555 <vinaysinghh7976@gmail.com>
2026-07-22 17:58:32 +0530

    Rename Static folder to static

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -127,7 +127,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
